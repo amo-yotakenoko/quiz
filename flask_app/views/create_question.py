fb16822b468c902c858ed73a1ad624871256987a
amo-yotakenoko <53008173+amo-yotakenoko@users.noreply.github.com>
2024-07-08 13:43:21 +0900

    問題作成選択問題

--- flask_app/views/create_question.py
+++ flask_app/views/create_question.py
@@ -3,14 +3,20 @@ from flask import Blueprint
 from flask_app import models
 from flask_app import db
 from flask_login import LoginManager, UserMixin, current_user, login_user, login_required, logout_user
-
+import json
 create_question_module = Blueprint("create_question", __name__)
 
 
 @create_question_module.route("/question/<id>",methods=['GET'])
 def create_question_get(id):
     question = models.Question.query.filter_by(questionid=id).one_or_none()
-    return render_template('create_question.html', id=id,question=  question)
+    selection =None
+    if(question.questionformat==1):
+        selection = json.loads(question.answer)
+
+    return render_template('create_question.html', id=id,question=  question,selection=selection)
+
+
 
 @create_question_module.route("/question/<id>",methods=['POST'])
 def create_question_post(id):
@@ -19,7 +25,18 @@ def create_question_post(id):
     if question == None:
         return Response(response="<h1>404 Not found<h1/>create_question_module", status=404)
     question.questiontext=request.form["question"]
-    question.answer=request.form["answer"]
+    print(request.form.get("type"),flush=True)
+    if(request.form.get("type")=="text"):
+        question.questionformat=0
+        question.answer=request.form["answer"]
+    if(request.form.get("type")=="choice"):
+        question.questionformat=1
+        question.answer=json.dumps({"choice_correct":request.form["choice_correct"],
+                                    "choice_incorrect1":request.form["choice_incorrect1"],
+                                    "choice_incorrect2":request.form["choice_incorrect2"],
+                                    "choice_incorrect3":request.form["choice_incorrect2"]})
+        
+    print(question.answer,flush=True)
     db.session.add(question)
     db.session.commit()
     print(question.questionsetid,flush=True)
